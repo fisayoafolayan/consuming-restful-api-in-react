614a276773abda93595f84d05af465ac54e3983c
Jonah Murray <jonahmurray321@gmail.com>
2020-06-18 14:09:46 -0500

    switched to vpce URL

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Recipes from './components/recipes';
 
+
 function refreshPage() {
         window.location.reload(false);
 }
@@ -24,7 +25,13 @@ class App extends Component {
 
 
     componentDidMount() {
-        fetch("https://thecocktaildb.com/api/json/v1/1/random.php")
+        fetch("https://vpce-0bd02e6e0e09e7544-4e9yt2ud-us-east-1d.execute-api.us-east-1.vpce.amazonaws.com", {
+            method: 'GET',
+            headers: {
+            'x-api-key': '7xggh6vvja',
+            'Content-Type': 'application/json'
+          }
+        })
             .then(res => res.json())
             .then((data) => {
                 this.setState({ recipes: data["drinks"] })
