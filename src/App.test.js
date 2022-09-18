8361cbe10a90045c985184eaeb2ec237139158a0
george <georgedimac@gmail.com>
2022-09-18 15:38:16 +0300

    upgraded to React 18
    ReactDOM.render is no longer supported in React 18. Use createRoot instead

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
 import App from './App';
+
+import { createRoot } from 'react-dom/client';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+  const root = createRoot(div);
+  root.render(<App tab="div" />);
+  root.unmount();
+
+  //ReactDOM.render(<App />, div);
+  //ReactDOM.unmountComponentAtNode(div);
 });
