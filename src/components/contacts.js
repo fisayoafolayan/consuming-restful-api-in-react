2e66e0721a22dd10e27cda0dad168bbd52e04990
george <georgedimac@gmail.com>
2022-09-18 15:47:56 +0300

    Update contacts.js
    fixed warning: Warning: Unknown DOM property class. Did you mean className?

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -5,11 +5,11 @@ const Contacts = ({contacts}) => {
         <div>
             <center><h1>Contact List</h1></center>
             {contacts.map((contact) => (
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">{contact.name}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">{contact.email}</h6>
-                        <p class="card-text">{contact.company.catchPhrase}</p>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">{contact.name}</h5>
+                        <h6 className="card-subtitle mb-2 text-muted">{contact.email}</h6>
+                        <p className="card-text">{contact.company.catchPhrase}</p>
                     </div>
                 </div>
             ))}
